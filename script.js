b0bf0cc7763bff3d61e4a7180f2de3f6fdd86925
AlkaTone <antonio.a.garces@gmail.com>
2020-05-02 23:10:05 -0700

    more changes made. not finished. planner doesn't change according to time & save button saves to console but gets deleted on refresh

--- script.js
+++ script.js
@@ -2,35 +2,50 @@ $(document).ready(function() {
     var currentDay = moment().format("dddd MMM Do YYYY");
     $("#currentDay").text("Plan for today's date: " + currentDay);
 
-    var currentTime = moment().format("HH");
+    var currentTime = moment().format("hh");
     console.log(currentTime);
     var timeBlocks = $(".row.time-block");
     console.log(timeBlocks.length);
-    function setCalendar() {
-        for (var i=0; i < timeBlocks.length; i++) {
-            var target = $(timeBlocks[i]);
-            var timeAttr = target.attr("time");
-            console.log(timeAttr);
+    function setWorkDay() {
+        for (var i = 0; i < timeBlocks.length; i++) {
+            var each = $(timeBlocks[i]);
+            var timeAttr = each.attr("time");
             if (currentTime < timeAttr) {
-                target.find(".description").addClass("past");
+                timeBlocks.attr("class", past);
             }
-            else if (currentTime == timeAttr) {
-                target.find(".description").addClass("present");
+            else if (currentTime === timeAttr) {
+                timeBlocks.attr("class", present);
             }
-            else {
-                target.find(".description").addClass("future");
+            else if (currentTime > timeAttr) {
+                timeBlocks.attr("class", future);
             }
-    }}
+    }
+}
 
     $(".saveBtn").on("click", function(event) {
         event.preventDefault();
-        var parent = $(this).prevAll(".description").val();
-        console.log(parent);
-        localStorage.setItem("event", JSON.stringify(parent));
-        $(".description").text("event");
+        var savedPlans = $(this).prevAll(".description").val();
+        console.log(savedPlans);
+        localStorage.setItem("event", JSON.stringify(savedPlans));
+        var getPlans = localStorage.getItem("event");
+        console.log(getPlans);
+        
+
+
+
+
+
+
+
+        // var savedPlans = $(this).prevAll(".description").val();
+        // console.log(savedPlans);
+        // localStorage.setItem("event", JSON.stringify(savedPlans));
+        // $(".description").text(savedPlans);
+        // var getPlans = localStorage.getItem(savedPlans)
+
     })
 
-    setCalendar();
+    setWorkDay();
 
 
 
